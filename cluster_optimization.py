8dfa2f8ddc20493726672466cdd081f824f0e4fb
Nama-gem <raphael.kaubruegger@gmail.com>
2026-09-21 15:19:11 -0600

    Run different optimizer

--- cluster_optimization.py
+++ cluster_optimization.py
@@ -36,11 +36,11 @@ LHS_SEED_BASE = None
 # method = "L-BFGS-B"
 # use_hessian = False
 
-# method = "SLSQP"
-# use_hessian = False
+method = "SLSQP"
+use_hessian = False
 
-method = "trust-constr"
-use_hessian = True
+# method = "trust-constr"
+# use_hessian = True
 
 options = {
     "maxiter": 1000,
@@ -100,7 +100,7 @@ WARM_START_OMEGA_DELTA_RATIOS = [
 #     only the warm-start optimum
 #
 # is optimized.
-WARM_START_INCLUDE_LHS = True
+WARM_START_INCLUDE_LHS = False
 
 
 # If True, rescale only Iz_echo and Ix_echo durations:
