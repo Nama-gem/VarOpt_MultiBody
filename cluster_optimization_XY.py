7e597f1a752be602ab02c2adaec0159b17ec9aff
Nama-gem <raphael.kaubruegger@gmail.com>
2026-09-24 10:25:27 -0600

    Add one random start so the data will start to be saved correctly

--- cluster_optimization_XY.py
+++ cluster_optimization_XY.py
@@ -35,7 +35,7 @@ OMEGA_DELTA_RATIOS = [
 
 MAX_LAYERS = 5
 
-N_INITIAL_CONDITIONS = 1000
+N_INITIAL_CONDITIONS = 1
 LHS_SEED_BASE = None
 
 
@@ -71,7 +71,7 @@ save_best = True
 #
 # Importantly, warm-start files are NOT loaded here.
 # They are loaded immediately before each optimization begins.
-WARM_START_INCLUDE_LHS = False
+WARM_START_INCLUDE_LHS = True
 
 
 # Rescale XY_echo durations using the ratio of single-pulse
